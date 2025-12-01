610bce6ff46166e15fc186b39068c32e9a0d6bff
satwinderstar <shineme70@gmail.com>
2025-12-01 20:39:09 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE = "wtank-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
